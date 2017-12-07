cf2932caea0d5d8b2d3f91284b761c66d07a988e
Tim Ambrose <luminoscity@gmail.com>
2017-12-07 10:59:23 -0800

    Started adding WheelRobot class

--- ReadDistances.m
+++
@@ -1,9 +0,0 @@
-% Tim Ambrose and Karthika Pai
-% 11 December 2017
-% EEC289A - UC Davis
-
-function distanceReadings = ReadDistances(x, y, orientation, envir, C)
-   distanceReadings = zeros(1, C.ANGLES, 'uint8');
-   % calculate distances based on obstacle definitions and angle that we
-   % are pointing
-end
