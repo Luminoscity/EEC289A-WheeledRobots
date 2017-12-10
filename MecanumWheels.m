3257505a20e6a637813609f6dc416351298ab5a0
Tim Ambrose <luminoscity@gmail.com>
2017-12-10 10:18:21 -0800

    Adjusted MecanumWheels to be able to test both mecanum and differential with new strategy (different world every episode). Worlds to be used are saved in NewWorlds.

--- MecanumWheels.m
+++ MecanumWheels.m
@@ -14,28 +14,26 @@
 %----------------------CONSTANTS---------------------------
 clear
 close all
-PLOT_WORLD = false;
+PLOT_WORLD = true;
 PLOT_INITIAL_DISTANCES = false;
-PROGRESS_STEP = 5;
+MECANUM = [true,false];              %false for differential wheels
+PROGRESS_STEP = 10;
 WORLD_IDX_START = 1;
+%MAX_PARAM = 15;
 DIST_QUANTIZE = 5;           % number of quantized distances
 DIST_QUANT_STEP = 5;         % centimeters between quantized distances
 DIST_ANGLE_MEAS = 60;        % number of angles to measure distances at
 DIST_ANGLE_QUANTIZE = 16;    % number of quantized distance angles to
                              % record as part of the state
 TURN_ANGLE = pi / 4;         % 45 degree turns
-WORLD_WIDTH = [10,10,10,11,11,11,12,12,12,13,13,13,14,14,14,15,15,15,16,16,16,17,18,19,20,21,22,23,24,25];
-WORLD_HEIGHT = [10,10,10,11,11,11,12,12,12,13,13,13,14,14,14,15,15,15,16,16,16,17,18,19,20,21,22,23,24,25];
 OBSTACLE_SEPARATION = 2;
-MIN_OBSTACLES = [1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2];
-MAX_OBSTACLES = [1,1,1,2,2,2,2,2,2,3,3,3,4,4,4,5,5,5,6,6,6,7,8,9,10,11,12,13,14,15];%15;%35;
-MAX_OBSTACLE_AREA = [10,10,10,12,12,12,14,14,14,16,16,16,18,18,18,20,20,20,22,22,22,24,26,28,30,32,34,36,38,40];
+MIN_OBSTACLES = 2;
 DISTANCE_SENSOR_RESOLUTION = 0.1;
 ROUND_MAGNITUDE = 1.49;
 REWARD_FLOOR = -200;
 
-EPISODES = 100;
-RUNS = 1;
+EPISODES = 200;  %max is 200
+RUNS = 3;
 
 EPSILON = 0.1;
 ALPHA = 0.5;
@@ -62,31 +60,28 @@ DIR_SOUTHWEST = 7 * pi / 4;
 
 directions = [DIR_WEST, DIR_NORTHWEST, DIR_NORTH, DIR_NORTHEAST,...
               DIR_EAST, DIR_SOUTHEAST, DIR_SOUTH, DIR_SOUTHWEST];
-           
-actions = [ACTION_NORTH; ACTION_LEFT; ACTION_RIGHT; ACTION_NORTHWEST;...
-           ACTION_WEST; ACTION_SOUTHWEST; ACTION_SOUTH;...
-           ACTION_SOUTHEAST; ACTION_EAST; ACTION_NORTHEAST];
+          
+for typeIndex = 1:2
+if MECANUM(typeIndex)
+   actions = [ACTION_NORTH; ACTION_LEFT; ACTION_RIGHT; ACTION_NORTHWEST;...
+              ACTION_WEST; ACTION_SOUTHWEST; ACTION_SOUTH;...
+              ACTION_SOUTHEAST; ACTION_EAST; ACTION_NORTHEAST];
+   wheelType = 'Mecanum';
+else
+   actions = [ACTION_NORTH; ACTION_LEFT; ACTION_RIGHT];
+   wheelType = 'Differential';
+end
 
 stateActionValues = zeros(DIST_ANGLE_QUANTIZE, DIST_ANGLE_QUANTIZE, ...
    length(directions), length(actions));
-%{
-stateActionValuesSarsa = stateActionValues(:,:,:,:);
-stateActionValuesQLearning = stateActionValues(:,:,:,:);
-%}
-stateActionValuesESarsa = stateActionValues(:,:,:,:);
 
-wIdx = 1;
-for world = WORLD_IDX_START:(WORLD_IDX_START + length(WORLD_WIDTH) - 1)
 
-fileString = sprintf('World%d-Mec.mat', world);
-load(fileString)
-worldString = sprintf('World%d', world);
 
 angleStep = 2 * pi / DIST_ANGLE_MEAS;
 angles = 0:angleStep:(2 * pi - angleStep);
 angleStep = 2 * pi / DIST_ANGLE_QUANTIZE;
 qAngles = 0:angleStep:(2 * pi - angleStep);
-
+%{
 C = struct('EPSILON', EPSILON,...
            'ALPHA', ALPHA,...
            'GAMMA', GAMMA,...
@@ -100,16 +95,16 @@ C = struct('EPSILON', EPSILON,...
            'qAngles', qAngles,...
            'DISTS', DIST_QUANTIZE,...
            'DIST_STEP', DIST_QUANT_STEP,...
-           'MAX_OBSTACLE_AREA', MAX_OBSTACLE_AREA(wIdx),...
-           'MIN_OBSTACLES', MIN_OBSTACLES(wIdx),...
-           'MAX_OBSTACLES', MAX_OBSTACLES(wIdx),...
+           'MAX_OBSTACLE_AREA', MAX_OBSTACLE_AREA,...
+           'MIN_OBSTACLES', MIN_OBSTACLES,...
+           'MAX_OBSTACLES', MAX_OBSTACLES,...
            'OBS_SPACE', OBSTACLE_SEPARATION,...
-           'WORLD_WIDTH', WORLD_WIDTH(wIdx),...
-           'WORLD_HEIGHT', WORLD_HEIGHT(wIdx),...
+           'WORLD_WIDTH', WORLD_WIDTH,...
+           'WORLD_HEIGHT', WORLD_HEIGHT,...
            'DIST_RES', DISTANCE_SENSOR_RESOLUTION,...
            'ROUND_MAGNITUDE', ROUND_MAGNITUDE);
-
-%------------------------MAIN---------------------------
+%}
+%--------------------------MAIN-----------------------------
 %{
 env = Environment(C);
 error = 1;
@@ -117,74 +112,40 @@ while error > 0
    error = env.GenerateObstacles();
 end
 %}
-if PLOT_WORLD
-   figure
-   imagesc(env.whichObstacles)
-   title(worldString);
-   set(gca, 'YDir', 'Normal')
-   colormap Jet;
-   colorbar;
-end
-
-robot = WheeledRobot(env);
-robot.StartAt(env.start, 0);
-
-if PLOT_INITIAL_DISTANCES
-   distances = robot.ReadDistances(env);
-   figure
-   for i = 1:C.ANGLES
-      polarplot([C.angles(i), (C.angles(i)+0.001)], [0, distances(i)])
-      hold on
-   end
-   [dirToGoal, angle] = robot.GoalBearing(env);
-   polarplot([angle, (angle+0.001)], [0, robot.distanceToGoal], '--k')
-   polarplot([directions(dirToGoal), (directions(dirToGoal)+0.001)], ...
-      [0, robot.distanceToGoal], 'k', 'LineWidth', 2)
-   hold off
-
-
-   qDistances = robot.QuantizedDistReadings(env);
-   figure
-   for i = 1:C.ANGLES
-      polarplot([C.angles(i), (C.angles(i)+0.001)], [0, qDistances(i)])
-      hold on
-   end
-   hold off
-end
-
-
-%{
-env.GenerateObstacles();
-figure
-imagesc(env.whichObstacles)
-colormap Jet
-colorbar;
-
-env.GenerateObstacles();
-figure
-imagesc(env.whichObstacles)
-colormap Jet
-colorbar;
-%}
-%stateActionValues = zeros(C.QUANT_ANGLES, C.QUANT_ANGLES, C.DIRS, ...
-%   C.ACTIONS);
 
 averageRange = 10;
 
 tstart = tic;
-%{
+
 rewardsSarsa = zeros(1, EPISODES);
 rewardsQLearning = zeros(1, EPISODES);
-%}
 rewardsESarsa = zeros(1, EPISODES);
-fprintf('World %d Runs remaining: ', world);
+fprintf('Runs:Episodes Remaining ');
+figure
 for run = 1:RUNS
-   fprintf('%d:', RUNS - run + 1);
+   fprintf(' %d:', RUNS - run + 1);
+   stateActionValuesSarsa = stateActionValues(:,:,:,:);
+   stateActionValuesQLearning = stateActionValues(:,:,:,:);
+   stateActionValuesESarsa = stateActionValues(:,:,:,:);
    for i = 1:EPISODES
       if mod(i - 1, PROGRESS_STEP) == 0
          fprintf('%d ', EPISODES - i + 1);
       end
-      %{
+      
+      worldString = sprintf('NewWorld%d', i);
+      load(strcat('NewWorlds/',worldString,'.mat'))
+      env.C.actions = actions;
+      env.C.ACTIONS = length(actions);
+      robot = WheeledRobot(env);
+      %robot.StartAt(env.start, 0);
+      
+      imagesc(env.whichObstacles)
+      title(worldString)
+      set(gca, 'YDir', 'Normal')
+      colormap Jet
+      colorbar
+      drawnow()
+      
       [Value, stateActionValuesSarsa] = Sarsa(stateActionValuesSarsa,...
          false, robot, env);
       robot.StartAt(env.start, 0);
@@ -194,40 +155,40 @@ for run = 1:RUNS
          stateActionValuesQLearning, robot, env);
       robot.StartAt(env.start, 0);
       rewardsQLearning(i) = rewardsQLearning(i) + max(Value, REWARD_FLOOR);
-      %}
+      
       [Value, stateActionValuesESarsa] = Sarsa(stateActionValuesESarsa,...
          true, robot, env);
       robot.StartAt(env.start, 0);
       rewardsESarsa(i) = rewardsESarsa(i) + max(Value, REWARD_FLOOR);
    end
 end
-%{
+
 rewardsSarsa = rewardsSarsa ./ RUNS;
 rewardsQLearning = rewardsQLearning ./ RUNS;
 sarsaResults = rewardsSarsa(:)';
 qLearningResults = rewardsQLearning(:)';
-%}
 rewardsESarsa = rewardsESarsa ./ RUNS;
 eSarsaResults = rewardsESarsa(:)';
 for i = (averageRange + 1):EPISODES
-   %{
    sarsaResults(i) = mean(rewardsSarsa((i - averageRange):i));
    qLearningResults(i) = mean(rewardsQLearning((i - averageRange):i));
-   %}
    eSarsaResults(i) = mean(rewardsESarsa((i - averageRange):i));
 end
-tstopSQ = tstop;
-tstopE = toc(tstart);
-fprintf('\nTime for World %d (%d runs, %d episodes): %f seconds\n', ...
-   world, RUNS, EPISODES, tstopE)
+
+tstop = toc(tstart);
+fprintf('\nTime for %s Wheels (%d runs, %d episodes): %f seconds\n', ...
+   wheelType, RUNS, EPISODES, tstop)
 
 %--------------------SAVE RESULTS--------------------------------
-saveString = sprintf('World%d.mat', world);
-
-save(saveString, 'env', 'sarsaResults', 'qLearningResults', 'eSarsaResults', ...
-   'EPISODES', 'RUNS', 'tstopE', 'tstopSQ')
-wIdx = wIdx + 1;
+if MECANUM(typeIndex)
+   saveString = sprintf('MecResults%dRuns%dWorlds.mat', RUNS, EPISODES);
+else
+   saveString = sprintf('DiffResults%dRuns%dWorlds.mat', RUNS, EPISODES);
 end
+C = env.C;
+save(saveString, 'C', 'sarsaResults', 'qLearningResults', 'eSarsaResults', ...
+   'EPISODES', 'RUNS', 'tstop')
+
 %--------------------PLOT RESULTS--------------------------------
 figure
 plot(sarsaResults)
@@ -237,8 +198,9 @@ plot(eSarsaResults)
 legend('Sarsa', 'Q-Learning', 'Expected Sarsa');
 xlabel('Episodes');
 ylabel('Sum of Rewards During Episode');
-str = strcat('Mecanum Wheels - ', worldString);
-title('Mecanum Wheels');
+str = sprintf('%s Wheels - %d Runs, %d Episodes', wheelType, RUNS, ...
+   EPISODES);
+title(str);
 hold off
-drawnow()
 
+end
