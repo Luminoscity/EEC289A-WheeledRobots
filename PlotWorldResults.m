bdbdd372ab46b037e312a6e3c2bd122bd24b2ab6
Tim Ambrose <luminoscity@gmail.com>
2017-12-10 10:49:49 -0800

    Finished function to plot results from multiple runs of old strategy (same world for a while)

--- PlotWorldResults.m
+++ PlotWorldResults.m
@@ -2,31 +2,41 @@
 % 11 December 2017
 % EEC289A - UC Davis
 
-WORLD_START = 24;
-WORLD_END = 28;
-
-for world = WORLD_START:WORLD_END
-loadString = sprintf('World%d-Mec.mat', world);
-load(loadString)
+WORLD_START = 25;
+WorldsToPlot = 3;
+Mecanum = true;
 figure
-subplot(1, 2, 1)
-imagesc(env.whichObstacles)
-worldString = sprintf('World%d', world);
-axis square
-title(worldString)
-set(gca, 'YDir', 'Normal')
-colormap Jet
-colorbar
 
-subplot(1, 2, 2)
-plot(sarsaResults)
-hold on
-plot(qLearningResults)
-plot(eSarsaResults)
-legend('Sarsa', 'Q-Learning', 'Expected Sarsa');
-xlabel('Episodes')
-ylabel('Sum of Rewards During Episode')
-str = strcat('Mecanum Wheels - ', worldString);
-title(str)
-hold off
+for world = WORLD_START:(WORLD_START + WorldsToPlot - 1)
+   if Mecanum
+      loadString = sprintf('SQE-Mec/MecWorld%d.mat', world);
+   else
+      loadString = sprintf('SQE-Diff/DiffWorld%d.mat', world);
+   end
+   
+   load(loadString)
+   subplot(WorldsToPlot, 2, (((world-WORLD_START) * 2)+1))
+   imagesc(env.whichObstacles)
+   worldString = sprintf('World%d', world);
+   axis square
+   title(worldString)
+   set(gca, 'YDir', 'Normal')
+   colormap Jet
+   colorbar
+
+   subplot(WorldsToPlot, 2, (((world-WORLD_START) * 2)+2))
+   plot(sarsaResults)
+   hold on
+   plot(qLearningResults)
+   plot(eSarsaResults)
+   legend('Sarsa', 'Q-Learning', 'Expected Sarsa');
+   xlabel('Episodes')
+   ylabel('Sum of Rewards During Episode')
+   if Mecanum
+      str = strcat('Mecanum Wheels - ', worldString);
+   else
+      str = strcat('Differential Wheels - ', worldString);
+   end
+   title(str)
+   hold off
 end
